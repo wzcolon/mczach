8ddee20b09e69906a5026291c13b8b33210de1fd
Zach Colon <zcolon80@gmail.com>
2013-09-03 21:35:33 -0400

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mczach::Application.config.secret_token = '32de26dfb1074c6db290c5b29332c5a5931018640554f8a811d0a59c4ac2cd67fc2493a8e78a8eb6c9b8eed8d73e268e2ab562fedb1ac8a7cb75e82369b0402d'
