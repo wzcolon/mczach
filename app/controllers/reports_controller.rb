476c137897df0929a8c15c6ecaf67edc580bc3bb
Zach Colon <zcolon80@gmail.com>
2013-09-06 02:26:59 -0400

    move averaging method to helper module

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,10 +1,10 @@
 class ReportsController < ApplicationController
 
   def show
-    @application = Application.find(params[:application_id])
-    @report_data = @application.request_logs.map { |log| [log.time_in_ruby, log.time_in_db, log.time_rendering]}
-    @time_in_ruby = time_in_ruby
-    @time_in_db   = time_in_db
+    @application    = Application.find(params[:application_id])
+    @report_data    = @application.request_logs.map { |log| [log.time_in_ruby, log.time_in_db, log.time_rendering]}
+    @time_in_ruby   = time_in_ruby
+    @time_in_db     = time_in_db
     @time_rendering = time_rendering
   end
 
@@ -21,15 +21,6 @@ class ReportsController < ApplicationController
   def time_rendering
     @application.request_logs.pluck(:time_rendering)
   end
-
-  def average_time(array)
-    sum = array.sum
-    length = array.length
-    sum / length
-  rescue
-    MathError 
-  end
-  helper_method :average_time
 
   MathError = Class.new(StandardError)
 end
